5aa52ef145d8643212679d2f33b72ec0465d3e0e
kosatkina <tatiana.kosatkina@gmail.com>
2025-03-22 21:38:40 -0400

    Created TabButton custom component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Fragment } from 'react';
 
 // Custom components
 import Header from "./components/Header.jsx";
+import TabButton from './components/TabButton.jsx';
 
 
 import './App.css';
@@ -11,6 +12,16 @@ function App() {
   return (
     <>
       <Header />
+      <main>
+        <section id="resume">
+          <menu>
+          <TabButton>Highlights</TabButton>
+          <TabButton>Resume</TabButton>
+          <TabButton>Personal Projects</TabButton>
+          <TabButton>Contact Info</TabButton>
+          </menu>
+        </section>
+      </main>
     </>
   );
 }
