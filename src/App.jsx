f4e9242ee5c99e377a7a9de0461972f41cf2dca5
kosatkina <tatiana.kosatkina@gmail.com>
2025-03-22 22:07:01 -0400

    Added props to TabButton component for dynamic content output and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,44 @@
 // React Hook
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 
 // Custom components
 import Header from "./components/Header.jsx";
 import TabButton from './components/TabButton.jsx';
-
+import { MENU } from './data.js';
 
 import './App.css';
 
 function App() {
+  // Call react hook to use state
+  const [selectedBtn, setSelectedBtn] = useState();
+  
+  // Function to highlight selected menu button
+  function handleSelectedBtn(selectedBtn) {
+    setSelectedBtn(selectedBtn);
+  }
+
   return (
     <>
       <Header />
       <main>
-        <section id="resume">
+        <section id="menu">
           <menu>
-          <TabButton>Highlights</TabButton>
-          <TabButton>Resume</TabButton>
-          <TabButton>Personal Projects</TabButton>
-          <TabButton>Contact Info</TabButton>
+          <TabButton isSelected={selectedBtn === 'highlights'}
+                    onSelect={function() {handleSelectedBtn('highlights')}}>Highlights</TabButton>
+          <TabButton isSelected={selectedBtn === 'resume'}
+                    onSelect={function() {handleSelectedBtn('resume')}}>Resume</TabButton>
+          <TabButton isSelected={selectedBtn === 'projects'}
+                    onSelect={function() {handleSelectedBtn('projects')}}>Personal Projects</TabButton>
+          <TabButton isSelected={selectedBtn === 'contact'}
+                    onSelect={function() {handleSelectedBtn('contact')}}>Contact Info</TabButton>
           </menu>
+          {!selectedTopic ? <p>Please select a topic.</p> : null} 
+          {selectedTopic ? (
+            <div id="tab_content">
+              <h3>{MENU[selectedBtn].title}</h3>
+              <p>{MENU[selectedBtn].description}</p>
+            </div>
+          ) : null}
         </section>
       </main>
     </>
---
+++ src/data.js
@@ -0,0 +1,18 @@
+export const MENU = {
+    highlights: {
+        title: 'Highlights',
+        description: 'in point form',
+    },
+    resume: {
+        title: 'Resume',
+        description: 'tech skills, education, work experience',
+    },
+    projects: {
+        title: 'Personal Projects',
+        description: 'achievement',
+    },
+    contact: {
+        title: 'Contact Info',
+        description: 'email, phone, linkedIn',
+    },
+};
