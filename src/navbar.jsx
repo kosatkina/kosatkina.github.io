61f87972a657981e4aae9dc03e7c195a1ca68efb
kosatkina <tatiana.kosatkina@gmail.com>
2025-04-15 09:31:57 -0400

    Added navbar from bootstrap

---
+++ src/navbar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+function Navbar() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#home">MyPortfolio</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                  aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+  
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="#home">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="https://www.linkedin.com/in/YOUR_PROFILE" target="_blank" rel="noopener noreferrer">
+                  LinkedIn
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="https://github.com/YOUR_USERNAME" target="_blank" rel="noopener noreferrer">
+                  GitHub
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+  
+  export default Navbar;
